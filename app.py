51193f0798f637455f6437c746eee76885d8eb40
Catherine Lopez <crlopezv@correo.url.edu.gt>
2025-05-23 18:17:17 -0600

    saludar

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hola, mundo")
+import tkinter as tk
+
+def saludar():
+    etiqueta.config(text="Hola, mundo")
+
+ventana = tk.Tk()
+ventana.title("Mi primer GUI")
+
+boton = tk.Button(ventana, text="Haz clic aquí", command=saludar)
+boton.pack()
+
+etiqueta = tk.Label(ventana, text="")
+etiqueta.pack()
+
+ventana.mainloop()
