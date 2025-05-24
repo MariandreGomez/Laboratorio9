be56ea4c7a0bf71849e63bd49071dd740037c97d
Mady <msdonis@correo.url.edu.gt>
2025-05-23 18:17:39 -0600

    Saludo

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hola, mundo")
+saludos = {
+    "Español": "Hola, mundo",
+    "Inglés": "Hello, world",
+    "Francés": "Bonjour, le monde",
+    "Alemán": "Hallo, Welt",
+    "Italiano": "Ciao, mondo",
+    "Japonés": "こんにちは世界",
+}
+
+for idioma, saludo in saludos.items():
+    print(f"{idioma}: {saludo}")
